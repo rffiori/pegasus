6701376444c021189823342fc3e81a259e99f736
Rodrigo Fiori <rodrigo.ffiori@gmail.com>
2023-06-17 19:47:11 +0200

    Setup: Json server

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'pegasus-client'`, () => {
+    it(`should have as title 'pegasus'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('pegasus-client');
+        expect(app.title).toEqual('pegasus');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('pegasus-client app is running!');
+        expect(compiled.querySelector('.content span')?.textContent).toContain('pegasus app is running!');
     });
 });
