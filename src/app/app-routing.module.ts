4fbf90dba812c8e76dfa0a3da9f66ea5e20312cf
Rodrigo Fiori <rodrigo.ffiori@gmail.com>
2023-06-17 20:15:00 +0200

    Adding Pricing page initial structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'pricing',
+        loadComponent: () => import('./pricing/pricing.component').then((mod) => mod.PricingComponent),
+    },
+    {
+        path: '',
+        redirectTo: 'pricing',
+        pathMatch: 'full',
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
