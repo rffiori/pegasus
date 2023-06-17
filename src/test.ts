de3232c0ed1f5ed561270ba03af4c22611167d97
Rodrigo Fiori <rodrigo.ffiori@gmail.com>
2023-06-17 21:45:31 +0200

    Adding ng mocks to make testing easier

---
+++ src/test.ts
@@ -0,0 +1,61 @@
+import { ngMocks } from 'ng-mocks';
+// auto spy
+ngMocks.autoSpy('jasmine');
+// in case of jest
+// ngMocks.autoSpy('jest');
+
+// In case, if you use @angular/router and Angular 14+.
+// You might want to set a mock of DefaultTitleStrategy as TitleStrategy.
+// A14 fix: making DefaultTitleStrategy to be a default mock for TitleStrategy
+import { DefaultTitleStrategy, TitleStrategy } from '@angular/router';
+import { MockService } from 'ng-mocks';
+ngMocks.defaultMock(TitleStrategy, () => MockService(DefaultTitleStrategy));
+
+// Usually, *ngIf and other declarations from CommonModule aren't expected to be mocked.
+// The code below keeps them.
+import { CommonModule } from '@angular/common';
+import { ApplicationModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+ngMocks.globalKeep(ApplicationModule, true);
+ngMocks.globalKeep(CommonModule, true);
+ngMocks.globalKeep(BrowserModule, true);
+
+// auto restore for jasmine and jest <27
+// declare const jasmine: any;
+import { MockInstance } from 'ng-mocks';
+jasmine.getEnv().addReporter({
+    specDone: MockInstance.restore,
+    specStarted: MockInstance.remember,
+    suiteDone: MockInstance.restore,
+    suiteStarted: MockInstance.remember,
+});
+
+// // If you use jest v27+, please add to its config testRunner=jest-jasmine2 for now.
+// // If you don't want to rely on jasmine at all, then, please,
+// // upvote the issue on github: https://github.com/facebook/jest/issues/11483.
+// // Once it has been merged you can use the code below.
+// // Also, please consider usage of MockInstance.scope instead.
+// import { addEventHandler } from 'jest-circus';
+// addEventHandler((event: { name: string }) => {
+//   switch (event.name) {
+//     case 'run_describe_start':
+//     case 'test_start':
+//       MockInstance.remember();
+//       break;
+//     case 'run_describe_finish':
+//     case 'run_finish':
+//       MockInstance.restore();
+//       break;
+//     default:
+//   }
+// });
+
+// // in case of mocha
+// mocha.setup({
+//   rootHooks: {
+//     afterAll: MockInstance.restore,
+//     afterEach: MockInstance.restore,
+//     beforeAll: MockInstance.remember,
+//     beforeEach: MockInstance.remember,
+//   },
+// });
